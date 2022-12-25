4fae0e9e69c08c8e7b92dad3b3730bd4389bcb15
Jeison Martinez <jeisonsmv@gmail.com>
2022-12-24 19:30:10 -0500

    Added module node usage

---
+++ NPM-Module/script-node.js
@@ -0,0 +1,9 @@
+//***** THIS IS THE WAY THROUHG ONLY NODE.JS  */
+
+// In order to test this project, you need to install the dependacies using the command "npm i";
+
+// If we need something from node_modules, we just type the name
+const uuid = require("uuid");
+
+// Run this with "node script-node.js"
+console.log(uuid.v4());
